1fee8e6eccf32237629404ce5d83eaca7a05ae0d
HelloYing <zy_xu@hotmail.com>
2014-11-21 13:23:49 -0500

    register

---
+++ src/edu/osu/CheckInput.java
@@ -0,0 +1,20 @@
+package edu.osu;
+
+public class CheckInput {
+	public boolean checkEmail(String email){
+		if(true){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean checkPassword(String password){
+		if(true){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
--- src/edu/osu/RegisterBean.java
+++ src/edu/osu/RegisterBean.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package edu.osu;
 
 import java.util.ArrayList;
@@ -8,135 +5,52 @@ import java.util.List;
 
 import javax.ejb.EJB;
 
-import examples.cse769.EJB.Service.HelloService;
+import examples.cse769.EJB.Service.PeopleService;
 
 /**
- * @author Reddiah
+ * @author Siwei Dng
  *
  */
 public class RegisterBean
 {
-	@EJB 
-	private HelloService helloService;
-    private String name;
-    private String password;
-    private String valid;
-    //radio
-    private String selection = "Employee";
-    //radio
-    
-    //test
-    private List<List<String>> jobs = new ArrayList<List<String>>();
-    
-
-	public List<List<String>> getJobs() {
-		if(jobs.size() == 0)
-		{
-		List<String> job1 = new ArrayList<String>();
-		List<String> job2 = new ArrayList<String>();
-		List<String> job3 = new ArrayList<String>();
-
-		job1.add("Developer");
-		job1.add("Java");
-		job1.add("80000");
-		
-		job2.add("Coder");
-		job2.add("C");
-		job2.add("60000");
-		
-		job3.add("Manager");
-		job3.add("Arch");
-		job3.add("120000");
-		
-		jobs.add(job1);
-		jobs.add(job2);
-		jobs.add(job3);
-
+	@EJB
+	private PeopleService peopleService=new PeopleService();
+	private String email;
+	private String password;
+	private String conpassword;
+	
+	public String getEmail(){
+		return email;
 	}
-		return jobs;
-
+	public void setEmail(String email){
+		this.email=email;
 	}
-
-	public void setJobs(List<List<String>> jobs) {
-		this.jobs = jobs;
+	public String getPassword(){
+		return password;
 	}
-    
-    
-
-    //test
-
-
-
-
-
-
-
-	//radio
-    public String getSelection() {
-		return selection;
+	public void setPassword(String password){
+		this.password=password;
 	}
-
-
-	public void setSelection(String selection) {
-		this.selection = selection;
+	public String getConpassword(){
+		return conpassword;
 	}
-//radio
-
-	public String getValid() {
-		return valid;
-	}
-
-
-	public void setValid(String valid) {
-		this.valid = valid;
-	}
-
-
-	public String getName ()
-    {
-        return name;
-    }
-
-
-    public void setName (final String name)
-    {
-		if(name.equalsIgnoreCase("!") || name.equalsIgnoreCase("@") || name.equalsIgnoreCase("#")
-				|| name.equalsIgnoreCase("$") || name.equalsIgnoreCase("%") || name.equalsIgnoreCase("^")
-				|| name.equalsIgnoreCase("&") || name.equalsIgnoreCase("*") || name.equalsIgnoreCase("(")
-				|| name.equalsIgnoreCase(")") || name.equalsIgnoreCase("_") || name.equalsIgnoreCase("+")
-				|| name.equalsIgnoreCase(""))
-			setValid("Not a Valid User Name");
-		else
-			setValid("Valid User Name");
-
-
-        this.name = name;
-    }
-
-
-    public String getPassword ()
-    {
-        return password;
-    }
-
-
-    public void setPassword (final String password)
-    {
-        this.password = password;
-    }
-
-
-	public String userName() {
-		if((name.isEmpty() != true) && (password.isEmpty()!= true)) {
-		String ret = helloService.userName(name, password, selection);
-		//helloService.password(password);
-		if(!ret.equalsIgnoreCase("Exists"))
-		return "true";
-		else 
-			return "Exists";
-		}
-		
-		else return "false";
+	public void setConpassword(String conpassword){
+		this.conpassword=conpassword;
 	}
 	
+	public String register(){
+		if(!password.equals(conpassword)){
+			return "Passwords are different!";
+		}
+		CheckInput checkInput=new CheckInput();
+		if(checkInput.checkEmail(email) && checkInput.checkPassword(password)){
+			return peopleService.insert(email, password);
+		}else if(!checkInput.checkEmail(email) && !checkInput.checkPassword(password)){
+			return "Please use a valid email and a valid password.";
+		}else if(!checkInput.checkEmail(email)){
+			return "Please use a valid email.";
+		}else{
+			return "Please use a valid password.";
+		}
+	}
 }
